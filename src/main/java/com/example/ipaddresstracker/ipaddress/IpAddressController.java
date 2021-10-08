2e2e86f71f2f6aecf6b4306cadc479a21de68ffc
Rondell <rondellkeionborel@gmail.com>
2021-10-08 08:16:51 -0400

    fixed duplicate markers on map

--- src/main/java/com/example/ipaddresstracker/ipaddress/IpAddressController.java
+++ src/main/java/com/example/ipaddresstracker/ipaddress/IpAddressController.java
@@ -1,44 +1,90 @@
 package com.example.ipaddresstracker.ipaddress;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-
-import org.json.JSONObject;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class IpAddressController{
 
-	String API_KEY = "at_nzakoX4zoRJs9atEiB51iKqZst93P";
-	String BASE_URL = "https://geo.ipify.org/api/v1?";
+	static final String API_KEY = "at_nzakoX4zoRJs9atEiB51iKqZst93P";
+	static final String BASE_URL = "https://geo.ipify.org/api/v1?";
+	static final String REQUIRED_URL = BASE_URL + "&apiKey=" + API_KEY;
 
 	@RequestMapping("/locate/defaultIP")
 	public String getDefaultIPAddress(){
 
-		URL urlToQuery;
-		HttpURLConnection connection = null;
-		JSONObject response = null;
 		StringBuilder builder = new StringBuilder();
+		URL urlToQuery = getUrl(null, null);
+
+		if(urlToQuery != null)
+			makeHttpRequest(builder, urlToQuery);
+
+		return builder.toString();
+	}
+
+	@RequestMapping("/locate/ipAddress/{ip}")
+	public String getIPAddressInfo(@PathVariable String ip){
+
+		StringBuilder builder = new StringBuilder();
+		String param = "&ipAddress";
+		URL urlToQuery = getUrl(ip, param);
+
+		if(urlToQuery != null)
+			makeHttpRequest(builder, urlToQuery);
+
+		return builder.toString();
+	}
+
+	@RequestMapping("/locate/domain/{domainName}")
+	public String getIPAddressInfoFromDomainName(@PathVariable String domainName){
+
+		StringBuilder builder = new StringBuilder();
+		String param = "&domain";
+		URL urlToQuery = getUrl(domainName, param);
+
+		if(urlToQuery != null)
+			makeHttpRequest(builder, urlToQuery);
+
+		return builder.toString();
+	}
+
+	private URL getUrl(String value, String param){
+
+		URL urlToQuery = null;
+		try{
+			if(param == null)
+				urlToQuery = new URL(REQUIRED_URL);
+			else
+				urlToQuery = new URL(REQUIRED_URL + param + "=" + value);
+		}
+		catch(MalformedURLException e){
+			e.printStackTrace();
+		}
+		return urlToQuery;
+	}
+
+	private void makeHttpRequest(StringBuilder builder, URL urlToQuery){
+
+		HttpURLConnection connection = null;
 
 		try{
-			urlToQuery  = new URL(BASE_URL + "&apiKey=" + API_KEY);
-			connection = (HttpURLConnection) urlToQuery.openConnection();
+			connection = (HttpURLConnection)urlToQuery.openConnection();
 			connection.setRequestMethod("GET");
 			connection.setRequestProperty("Accept", "application/json");
 			connection.connect();
 
 			if(connection.getResponseCode() == 200
-			|| connection.getResponseCode() == 201){
+					|| connection.getResponseCode() == 201){
 
 				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 				String line;
@@ -55,16 +101,5 @@ public class IpAddressController{
 			if(connection != null)
 				connection.disconnect();
 		}
-		return builder.toString();
-	}
-
-	@RequestMapping("/locate/ipAddress/{ip}")
-	public String getIPAddressInfo(@PathVariable String ip){
-		return "ip info";
-	}
-
-	@RequestMapping("/locate/domain/{domainName}")
-	public String getIPAddressInfoFromDomainName(@PathVariable String domainName){
-		return "domain info";
 	}
 }
--- src/main/resources/static/javascript/script.js
+++ src/main/resources/static/javascript/script.js
@@ -1,41 +1,61 @@
-let myForm = document.querySelector(".ipAddressForm");
-let submit = document.querySelector('.submitButton');
+let myFormElement = document.getElementById("ipAddressForm");
+let submitElement = document.getElementById('submitButton');
 
-let ip = document.getElementById("ipAddress");
-let location = document.getElementById("location");
-let timezone = document.getElementById("timezone");
-let isp = document.getElementById("isp");
+let ipElement = document.getElementById("ipAddress");
+let locationElement = document.getElementById("location");
+let timezoneElement = document.getElementById("timezone");
+let mapElement = document.getElementById("isp");
 
-let map = L.map('mapid');
+let map;
 let marker;
+let isMapInitialized = false;
+let isMarkerInitialized = false;
 
 console.log("Starting");
 
 document.addEventListener('DOMContentLoaded', ()=>{
-    submit.addEventListener('click', getIpOrDomain, false);
+    submitElement.addEventListener('click', getIpOrDomain, false);
 }, false);
 
 function getIpOrDomain(e){
     e.preventDefault();
-    let formData = new FormData(myForm);
-    let ipInfo = getIPInfo(formData);
-    updateElements(ipInfo);
-    updateMap(ipInfo);
+    let formData = new FormData(myFormElement);
+    getIPInfo(formData.get('ip'));
 }
 
 function isIPAddress(ipAddress) {
-    console.log("checking ip");
-    let regEx = new RegExp('^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$');
-    return regEx.test(ipAddress);
+    return /^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$/.test(ipAddress);
 }
 
 function isDomainName(domainName) {
-    console.log("checking domain");
-    let regEx = new RegExp('/^(?!:\\/\\/)([a-zA-Z0-9]+\\.)?[a-zA-Z0-9][a-zA-Z0-9-]+\\.[a-zA-Z]{2,6}?$/i');
-    return regEx.test(domainName);
+    return /^(?!:\/\/)([a-zA-Z0-9]+\.)?[a-zA-Z0-9][a-zA-Z0-9-]+\.[a-zA-Z]{2,6}?$/i.test(domainName);
 }
 
-async function getIPInfo(formData = null){
+function sendHttpRequest(url) {
+    let val;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url);
+
+    xhr.onload = function () {
+
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+
+            if (xhr.status === 200
+                && xhr.responseText !== null
+                && xhr.responseText.length > 0) {
+                val = JSON.parse(xhr.responseText);
+                updateElements(val);
+                updateMap(val);
+            }
+            else {
+                alert("There was an error serving your request, response was: " + xhr.responseText);
+            }
+        }
+    }
+    xhr.send();
+}
+
+function getIPInfo(formData = null){
     console.log("getting ip");
     let url = "/locate";
 
@@ -52,51 +72,61 @@ async function getIPInfo(formData = null){
     else{
         url += "/defaultIP";
     }
-    let response = await fetch(url);
-    return response.json();
+    sendHttpRequest(url);
 }
 
 function updateMap(data){
-    //TODO update map with location information
-}
-
-function updateElements(ipInfo) {
-    console.log("updating elements");
-    ip.innerText = ipInfo.ip;
-    location.innerText = ipInfo.location.city + " " + ipInfo.location.region;
-    timezone.innerText = "UTC " + ipInfo.location.timezone;
-    isp.innerText = ipInfo.isp;
-}
-
-function initPage() {
-    console.log("initializing page");
-    updateElements(getIPInfo());
+    let lat = data.location.lat;
+    let long = data.location.lng;
+    setMap(lat, long, 13);
+    updateMarker(lat, long);
 }
 
 function setMap(lat, long, zoomLevel){
-    return map.setView([lat, long], zoomLevel);
+
+    if (map)
+        map.flyTo([lat, long], zoomLevel);
+    else {
+        initMap();
+        map.setView([lat, long], zoomLevel);
+        isMapInitialized = true;
+    }
+}
+
+function initMap() {
+    map = L.map('mapid');
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+        maxZoom: 20,
+        tileHeight: 520,
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
+
+function updateMarker(lat, long) {
+
+    if(marker)
+        marker.setLatLng([lat, long]);
+    else {
+        marker = getMarker(lat, long, map);
+        isMarkerInitialized = true;
+    }
 }
 
 function getMarker(lat, long, myMap){
     return L.marker([lat, long]).addTo(myMap);
 }
 
+function updateElements(ipInfo) {
+    console.log("updating elements");
+    ipElement.innerText = ipInfo.ip;
+    locationElement.innerText = ipInfo.location.city + " " + ipInfo.location.region;
+    timezoneElement.innerText = "UTC " + ipInfo.location.timezone;
+    mapElement.innerText = ipInfo.isp;
+}
+
+function initPage() {
+    console.log("initializing page");
+    getIPInfo();
+}
+
 initPage();
-
-// map layer
-L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-    maxZoom: 20,
-    tileHeight: 520,
-    attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-}).addTo( map );
-
-// popups for objects
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-/* popup as layer
-let popup = L.popup()
-    .setLatLng([51.5, -0.09])
-    .setContent("I am a standalone popup.")
-    .openOn(map);
-
- */
